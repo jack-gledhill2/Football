f4abd2f2a787730fe4d4b2815abaf99d7cc26f52
Jon Pankhurst <jon.pankhurst@photo4me.com>
2013-03-04 12:42:43 +0000

    my save

--- Football/PractiseMatch.vb
+++ Football/PractiseMatch.vb
@@ -3,13 +3,13 @@
 Module PractiseMatch
     Sub Main()
         Dim Team As New Team()
-        'Dim FilteredList As IEnumerable(Of Player)
+        Dim FilteredList As IEnumerable(Of Player)
 
-        'FilteredList = From t In Team.ListOfPlayers Where t.Name Like "Jack Gledhill"
+        FilteredList = From t In Team._ListOfPlayers Where t.Name Like "Jack Gledhill"
 
-        'For Each Player In FilteredList
-        'Console.WriteLine(Player.Name)
-        ' Next
+        For Each Player In FilteredList
+            Console.WriteLine(Player.Name)
+        Next
     End Sub
 
 
