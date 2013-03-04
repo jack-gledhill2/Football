93c9b1888ac5687364e69fea407a3cd10935390c
Jack Gledhill <jack.gledhill@photo4me.com>
2013-03-04 15:31:16 +0000

    random pass

--- Football/PractiseMatch.vb
+++ Football/PractiseMatch.vb
@@ -8,6 +8,7 @@ Module PractiseMatch
 
         MyManager.TeamList(PlayerPossesion).Possession = True
 
+        ' match starts
         For Each Player In MyManager.TeamList
             If Player.Possession Then
                 Console.WriteLine(CStr(Player.ShirtNumber) & " " & Player.Name & " ON THE BALL!")
@@ -16,12 +17,23 @@ Module PractiseMatch
             End If
         Next
 
+        'gill' keep possesssion for 11 passes
+        For a = 0 To 10
+            'take ball away
+            MyManager.TeamList(PlayerPossesion).Possession = False
+            'pick a new player to pass to
+            PlayerPossesion = RndPosession()
+            MyManager.TeamList(PlayerPossesion).Possession = True
+            'Display the player
+            Console.WriteLine(CStr(MyManager.TeamList(PlayerPossesion).ShirtNumber) & " " & MyManager.TeamList(PlayerPossesion).Name & " ON THE BALL!")
+        Next
+
     End Sub
 
 
     Public Function RndPosession() As Integer
         Dim Generator As System.Random = New System.Random()
-        Return Generator.Next(1, 11)
+        Return Generator.Next(0, 10)
     End Function
 
 End Module
